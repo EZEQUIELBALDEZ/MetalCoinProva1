0de39c245894efd5c5d81e68b9e0bfe56d9b1ffc
Senai <Senai@DESKTOP-1H6SOH0>
2024-05-17 21:40:19 -0300

    feat : Implementacao do controller

--- CORE/MetalCoin.Application/Services/CupomService.cs
+++ CORE/MetalCoin.Application/Services/CupomService.cs
@@ -2,12 +2,14 @@
 using Metalcoin.Core.Dtos.Categorias;
 using Metalcoin.Core.Dtos.Request;
 using Metalcoin.Core.Dtos.Response;
+using Metalcoin.Core.Enums;
 using Metalcoin.Core.Interfaces.Repositories;
 using Metalcoin.Core.Interfaces.Services;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,13 +23,112 @@ namespace MetalCoin.Application.Services
             _cupomRepository = repository;
         }
 
+        public async Task<CupomResponse> AtualizarCupom(CupomAtualizarRequest cupom)
+        {
+            var cupomDb = await _cupomRepository.ObterPorId(cupom.Id);
+
+            cupomDb.CodigoDoCupom = cupom. CodigoDoCupom.ToUpper();
+            cupomDb.DescricaoDoCupom = cupom.DescricaoDoCupom;
+            cupomDb.ValorDoDesconto = cupom.ValorDoDesconto;
+
+            cupomDb.TipoDescontoCupom = cupom.TipoDescontoCupom;
+            cupomDb.TipoStatusCupom = cupom.TipoStatusCupom;
+
+            cupomDb.DataDeValidade = cupom.DataDeValidade;
+            cupomDb.CuponsLiberados = cupom.CuponsLiberados;
+
+
+            await _cupomRepository.Atualizar(cupomDb);
+
+
+            var response = new CupomResponse
+            {
+   
+
+                Id = cupomDb.Id,
+                CodigoDoCupom = cupomDb.CodigoDoCupom,
+                DescricaoDoCupom = cupomDb.DescricaoDoCupom,
+                ValorDoDesconto = cupomDb.ValorDoDesconto,
+
+                TipoDescontoCupom = cupomDb.TipoDescontoCupom,
+                TipoStatusCupom = cupomDb.TipoStatusCupom,
+
+                DataDeValidade = cupomDb.DataDeValidade,
+                CuponsLiberados = cupomDb.CuponsLiberados,
+                CuponsUsados = cupomDb.CuponsUsados
+
+
+            };
+
+            return response;
+        }
+
+        public Task<CupomResponse> CadastrarCupom(CupomCadastrarRequest cupom)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<CupomResponse> CupomCadastrar(CupomCadastrarRequest cupom)
+        {
+
+            if (cupom.DataDeValidade < DateTime.Today)
+            {
+
+                throw new ArgumentException
+                    ("A data da validade só pode ser posterior a data atual! ");
+            }
+
+            var cupomEntidade = new Cupom
+            {
+
+                CodigoDoCupom = cupom.CodigoDoCupom,
+                DescricaoDoCupom = cupom.DescricaoDoCupom,
+                ValorDoDesconto = cupom.ValorDoDesconto,
+
+                TipoDescontoCupom = cupom.TipoDescontoCupom,
+                TipoStatusCupom = cupom.TipoStatusCupom,
+
+                DataDeValidade = cupom.DataDeValidade,
+
+            };
+
+
+            await _cupomRepository.Adicionar(cupomEntidade);
+
+            var response = new CupomResponse
+            {
+
+                Id = cupomEntidade.Id,
+                CodigoDoCupom = cupomEntidade.CodigoDoCupom,
+                DescricaoDoCupom = cupomEntidade.DescricaoDoCupom,
+                ValorDoDesconto = cupomEntidade.ValorDoDesconto,
+
+                TipoDescontoCupom = cupomEntidade.TipoDescontoCupom,
+                TipoStatusCupom = cupomEntidade.TipoStatusCupom,
+
+                DataDeValidade = cupomEntidade.DataDeValidade,
+                CuponsLiberados = cupomEntidade.CuponsLiberados,
+                CuponsUsados = cupomEntidade.CuponsUsados
 
 
 
+            };
 
 
+            return response;
 
+        }
 
+        public async Task<bool> DeletarCupom(Guid id)
+        {
+
+            var cupom = await _cupomRepository.ObterPorId(id);
+            if (cupom == null) return false;
+
+            await _cupomRepository.RemoverCupom(id);
+            return true;
 
         }
     }
